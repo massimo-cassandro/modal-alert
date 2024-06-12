aa49ebf75ad7e38059caf3d1c5ff692296428f1f
Massimo Cassandro <massimo.cassandro@gmail.com>
2024-06-12 15:34:02 +0200

    v.2.1.2 - css upd + upd stylelint / eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,12 @@
+import eslint_config from '@massimo-cassandro/eslint-config';
+
+export default [
+  ...eslint_config,
+  {
+    files: ['src/*.js'],
+    ignores: [
+      'dist/**/*.js'
+    ],
+  }
+];
+
